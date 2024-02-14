30008c3c42cf593134dbd63b837202902857de74
budi-afk <budiman1347@gmail.com>
2024-02-14 22:37:53 +0700

    client side filtering cabin

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -4,14 +4,27 @@ import CabinRow from "./CabinRow";
 import Spinner from "../../ui/Spinner";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
+import { useSearchParams } from "react-router-dom";
 
 export default function CabinTable() {
   const { isLoading, data, error } = useQuery({
     queryKey: ["cabins"],
     queryFn: getCabins,
   });
+  const [searchParams] = useSearchParams();
 
   if (isLoading) return <Spinner />;
+
+  const filterValue = searchParams.get("discount") || "all";
+
+  let filteredData;
+  if (filterValue === "all") filteredData = data;
+
+  if (filterValue === "no-discount")
+    filteredData = data.filter((item) => item.discount === 0);
+
+  if (filterValue === "with-discount")
+    filteredData = data.filter((item) => item.discount > 0);
 
   return (
     <Menus>
@@ -24,7 +37,7 @@ export default function CabinTable() {
           <div>Discount</div>
         </Table.Header>
         <Table.Body
-          data={data}
+          data={filteredData}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
