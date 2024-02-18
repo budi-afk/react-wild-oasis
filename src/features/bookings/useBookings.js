2893e475fdc980be66ef0816eebf139de182e6b2
budi-afk <budiman1347@gmail.com>
2024-02-18 22:21:53 +0700

    sorting bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -5,18 +5,25 @@ import { useSearchParams } from "react-router-dom";
 export function useBookings() {
   const [searchParams] = useSearchParams();
 
+  // FILTERING
   const filteredValue = searchParams.get("status");
   const filter =
     !filteredValue || filteredValue === "all"
       ? null
       : { field: "status", value: filteredValue };
+
+  // SORTING
+  const sortedValue = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortedValue.split("-");
+  const sortBy = { field, direction };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings", filter],
-    queryFn: () => getBookings({ filter }),
+    queryKey: ["bookings", filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
 
   return { isLoading, bookings, error };
--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,15 +1,21 @@
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
-export async function getBookings({ filter }) {
+export async function getBookings({ filter, sortBy }) {
   let query = supabase
     .from("bookings")
     .select(
       "id,created_at, startDate, endDate, numNights, numGuests, totalPrice, status, guests(email, fullName), cabins(name)"
     );
 
-  if (filter !== null) {
+  if (filter) {
     query = query[filter.method || "eq"](filter.field, filter.value);
+  }
+
+  if (sortBy) {
+    query = query.order(sortBy.field, {
+      ascending: sortBy.direction === "asc",
+    });
   }
 
   const { data, error } = await query;
